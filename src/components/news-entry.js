ca6e01ea7c67dec9ddcb71f70ff574199a49837b
Jo Lou <j.lou123@hotmail.com>
2020-01-11 01:48:31 -0500

    timeline complete, will add photos later when needed

--- src/components/news-entry.js
+++ src/components/news-entry.js
@@ -1,16 +1,16 @@
 import React from "react"
 import entryStyles from "./news-entry.module.css"
 
-export default () => (
+export default (props) => (
 <div className={entryStyles.beside}>
   <div class={entryStyles.container}>
       <div class={entryStyles.left}>
-        <h2>2017</h2>
+        <div className={entryStyles.dateText}>{props.date}</div>
       </div>
     </div>
   <div className={`${entryStyles.content} ${entryStyles.right}`}>
-    <h2>Title</h2>
-    <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    <div className={entryStyles.titleText}>{props.title}</div>
+    <p>{props.content}</p>
   </div>
 </div>
 )
--- src/pages/news.js
+++ src/pages/news.js
@@ -5,10 +5,24 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
+    <div style={{height: 100}}></div> {/*just for extra spacing*/}
     <div className={newsStyles.timeline}>
-      <NewsEntry />
-      <NewsEntry />
-      <NewsEntry />
+      <NewsEntry 
+        date="Jan 11, 2020"
+        title="Hello, World!"
+        content="Website is now live!"
+      />
+      <NewsEntry 
+        date="Jan 11, 2020"
+        title="Hello, World!"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
+      <NewsEntry 
+        date="Jan 11, 2020"
+        title="Hello, World!"
+        content="Website is now live!"
+      />
     </div>
+    <div style={{height: 100}}></div> {/*just for extra spacing*/}
   </Layout>
 )
